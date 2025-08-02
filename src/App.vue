a92967880ebacb093d5d97380f2cdd8ad91d0318
Doomer77 <lfa1925@rambler.ru>
2025-08-02 16:54:16 +0300

    add events fliped and checked

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <!-- <template>{{ date.toString() }}</template> -->
-<template><Button /></template>
+<template>
+  <main class="main">
+    <Header />
+    <section class="section">
+      <Button type="main" />
+      <div class="card-section">
+        <Card type="front" number="01" name="dust-coat" />
+        <Card type="fliped" number="02" name="караван верблюдов" />
+      </div>
+    </section>
+  </main>
+</template>
 
 <script setup>
 // const date = new Date();
 import Button from "./components/Button.vue";
+import Header from "./components/Header.vue";
+import Card from "./components/Card.vue";
 </script>
 
 <style scoped></style>
